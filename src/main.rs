eb403abab2bee76caa2311c0287e01569a57433d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-03-25 23:49:31 +0100

    feat: add base for `/api` GET route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ApiIndex {
+    server_version: String,
+}
+
+#[get("/api")]
+async fn index() -> impl Responder {
+    let obj = ApiIndex {
+        server_version: "0.0-alpha".to_string(),
+    };
+
+    HttpResponse::Ok().json(obj)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
